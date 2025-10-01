904c044daa390c041dae34f35ec3c9931d1ad93e
Brigham Andersen <brighambandersen@gmail.com>
2025-10-01 06:25:57 +0000

    Switch deployment to cp

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,7 +5,7 @@ echo "Deploying portfolio"
 
 # nginx
 
-sudo ln -sf /home/brig/code/portfolio/deploy/nginx.conf /etc/nginx/conf.d/root.conf
+sudo cp /home/brig/code/portfolio/deploy/nginx.conf /etc/nginx/conf.d/root.conf
 
 sudo nginx -t
 sudo systemctl reload nginx
